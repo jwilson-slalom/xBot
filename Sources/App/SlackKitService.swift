b289d1edb26845b81b509f0f9c5994f14f1fa78c
Jacob Wilson <jacob.wilson@slalom.com>
2019-02-22 17:00:07 -0600

    Refactor SlackKitService to be a initiated from a Provider and have access to Vapor Container class

--- Sources/App/SlackKitService.swift
+++ Sources/App/SlackKitService.swift
@@ -9,16 +9,14 @@ import Vapor
 import SlackKit
 import Starscream
 
-class SlackKitService: Service {
+final class SlackKitService {
     var bot: SlackKit
 
     init() {
         bot = SlackKit()
-
-        registerRTMConnection()
     }
 
-    private func registerRTMConnection() {
+    public func registerRTMConnection(with container: Container) {
         bot.addWebAPIAccessWithToken("xoxp-548586128101-547645124752-557599275843-7f43af1b0a56c236382e22e3f478ca0b")
         bot.addRTMBotWithAPIToken("xoxb-548586128101-557599279299-ZLx0RxuUKQw6vbkPtVsXN0WQ")
 
@@ -39,5 +37,23 @@ class SlackKitService: Service {
                 print("Error Sending Message: \(error)")
             }
         }
+    }
+}
+extension SlackKitService: ServiceType {
+    static func makeService(for container: Container) throws -> SlackKitService {
+        return .init()
+    }
+}
+
+public final class SlackKitProvider: Provider {
+    public func register(_ services: inout Services) throws {
+        services.register(SlackKitService.self)
+    }
+
+    public func didBoot(_ container: Container) throws -> EventLoopFuture<Void> {
+        let slackKitService = try container.make(SlackKitService.self)
+        slackKitService.registerRTMConnection(with: container)
+
+        return .done(on: container)
     }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -30,6 +30,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 
-    let slackService = SlackKitService()
-    services.register(slackService)
+    try services.register(SlackKitProvider())
 }
