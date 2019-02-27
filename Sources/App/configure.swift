b533a0ea96b2fe734cdcadd56a5c699a379affa8
Jacob Wilson <jacob.wilson@slalom.com>
2019-02-26 23:28:02 -0600

    Added in check to ensure that .message is coming back from not a bot so there is not an infinite loop

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -27,9 +27,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 	let config: PostgreSQLDatabaseConfig
 	if let databaseUrl = Environment.get("DATABASE_URL"), let herokuConfig = PostgreSQLDatabaseConfig(url: databaseUrl, transport: .unverifiedTLS) {
 		config = herokuConfig
-		print("Using Heroku Config")
+		print("Using Heroku PostgreSQL Config")
 	} else {
-		print("Using Local Config")
+		print("Using Local PostgreSQL Config")
 		config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "postgres", database: "xbot", password: nil, transport: .cleartext)
 	}
 
