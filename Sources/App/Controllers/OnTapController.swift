ffa08b99ebe9af99a9182fdee1f6f76d9dad7433
Allen Humphreys <allen.humphreys@icloud.com>
2019-02-28 17:40:59 -0600

    Move message handling into the controllers that care (#14)

--- Sources/App/Controllers/OnTapController.swift
+++ Sources/App/Controllers/OnTapController.swift
@@ -8,6 +8,8 @@
 import Vapor
 import HTTP
 import Routing
+import class SlackKit.Event
+import enum SlackKit.EventType
 
 final class OnTapController: RouteCollection, ServiceType {
 
@@ -27,6 +29,18 @@ final class OnTapController: RouteCollection, ServiceType {
 
     func update(req: Request, content: Beer) throws -> EmptyCollection<Void> {
         return EmptyCollection<Void>()
+    }
+}
+
+extension OnTapController: SlackHandler {
+
+    var eventTypes: [EventType] { return [.message] }
+
+    func handleEvent(event: Event, slack: SlackMessageSender) {
+
+        if event.message?.text?.contains("beer") ?? false {
+            try! slack.sendMessage(text: "Maybe you should try one of these beers...", channelId: event.channel!.id!)
+        }
     }
 }
 
--- Sources/App/Providers/SlackKit/SlackKitService.swift
+++ Sources/App/Providers/SlackKit/SlackKitService.swift
@@ -8,55 +8,65 @@
 import Vapor
 import SlackKit
 
+protocol SlackHandler {
+    func handleEvent(event: Event, slack: SlackMessageSender)
+    var eventTypes: [EventType] { get }
+}
+
 final class SlackKitService {
     private let bot: SlackKit
     private let apiKeyStorage: APIKeyStorage
-    private let karmaRepository: KarmaRepository
+    private var handlers = [SlackHandler]()
 
     init(_ apiKeyStorage: APIKeyStorage,
-         karmaRepository: KarmaRepository) {
+         // We're capturing the container here, but a more flexible architure
+         // would follow the routing patterns used by the the vapor HTTP service
+         container: Container) throws {
 
         self.bot = SlackKit()
         self.apiKeyStorage = apiKeyStorage
-        self.karmaRepository = karmaRepository
+
+        handlers.append(try container.make(OnTapController.self))
     }
 
     public func registerRTMConnection() {
         bot.addRTMBotWithAPIToken(apiKeyStorage.botUserApiKey, rtm: VaporEngineRTM())
 
-        bot.notificationForEvent(.message) { event, clientConnection in
-            guard let connection = clientConnection else {
-                print("Bad ClientConnection")
-                return
-            }
+        let eventTypes: [EventType] =
+        [
+            .message,
+            .channelJoined
+        ]
 
-            guard let channelId = event.channel?.id, event.message?.botID == nil else {
-                print("Bad Channel Id")
-                return
-            }
+        for type in eventTypes {
+            bot.notificationForEvent(type) { (event, connection) in
+                guard let connection = connection else { return }
 
-            if let message = event.text {
-                let karmaParser = KarmaParser()
-                let captureGroups = karmaParser.captureGroupsFrom(message: message)
-
-                var outgoingMessage = ""
-                for group in captureGroups {
-                    outgoingMessage.append("\(group) | ")
-                }
-
-                do {
-                    guard !outgoingMessage.isEmpty else {
-                        return
-                    }
-                    try self.sendMessage(using: connection, text: outgoingMessage, channelId: channelId)
-                } catch {
-                    print("Error Sending Message: \(error)")
-                }
+                self.handleEvent(event, onConnection: connection)
             }
         }
     }
 
-    private func sendMessage(using clientConnection: ClientConnection, text: String, channelId: String) throws {
+    private func handleEvent(_ event: Event, onConnection connection: ClientConnection) {
+        guard let type = event.type else {
+            print("Unhandled event type")
+            return
+        }
+        handlers
+            .filter { $0.eventTypes.contains(type) }
+            .forEach { $0.handleEvent(event: event, slack: SlackMessageSender(clientConnection: connection)) }
+    }
+}
+
+public struct SlackMessageSender {
+
+    private let clientConnection: ClientConnection
+
+    init(clientConnection: ClientConnection) {
+        self.clientConnection = clientConnection
+    }
+
+    public func sendMessage(text: String, channelId: String) throws {
         guard let rtm = clientConnection.rtm else { throw Abort(.internalServerError) }
 
         try rtm.sendMessage(text, channelID: channelId)
@@ -64,10 +74,10 @@ final class SlackKitService {
 }
 
 extension SlackKitService: ServiceType {
+
     static public func makeService(for container: Container) throws -> SlackKitService {
         let apiKeyStorage = try container.make(APIKeyStorage.self)
-        let karmaRepository = try container.make(KarmaRepository.self)
 
-        return .init(apiKeyStorage, karmaRepository: karmaRepository)
+        return try .init(apiKeyStorage, container: container)
     }
 }
