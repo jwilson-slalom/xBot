5b01c76d05e2026438b631ff2b40e40040fac321
Allen Humphreys <allen.humphreys@icloud.com>
2019-03-01 10:59:30 -0600

    Beers support 🎊

--- Sources/App/Providers/SlackKit/SlackKitProvider.swift
+++ Sources/App/Providers/SlackKit/SlackKitProvider.swift
@@ -7,13 +7,22 @@
 
 import Vapor
 
-public struct SlackKitProvider: Provider {
+fileprivate(set) var slackKitService: SlackKitService!
+
+public final class SlackKitProvider: Provider {
+
+
     public func register(_ services: inout Services) throws {
         services.register(SlackKitService.self)
     }
 
     public func didBoot(_ container: Container) throws -> EventLoopFuture<Void> {
         let service = try container.make(SlackKitService.self)
+        slackKitService = service
+
+        service.handlers.append(try container.make(OnTapController.self))
+        service.handlers.append(try container.make(KarmaController.self))
+
         service.registerRTMConnection()
 
         return .done(on: container)
