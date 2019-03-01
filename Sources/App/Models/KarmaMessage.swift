8e9495187aa8a7fb77b7a42b569d20a66218f078
Ryan Jones <ryan.jones@slalom.com>
2019-02-28 18:37:24 -0600

    Create Karma message

---
+++ Sources/App/Models/KarmaMessage.swift
@@ -0,0 +1,21 @@
+//
+//  KarmaMessage.swift
+//  App
+//
+//  Created by Ryan Jones on 2/28/19.
+//
+
+import Foundation
+
+struct KarmaMessage {
+    let user: String
+    let karma: Int
+}
+
+extension KarmaMessage {
+
+    func slackUser() -> String {
+        return "<@\(user)>"
+    }
+
+}
